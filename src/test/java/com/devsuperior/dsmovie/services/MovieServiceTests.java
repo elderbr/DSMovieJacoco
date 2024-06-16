903c69eae813ab67e59252d8f1099961cf7d057e
ElderBR <elderbarbosaromao@gmail.com>
2024-06-16 17:21:05 -0300

    Criado o teste findByIdShouldReturnMovieDTOWhenIdExists

--- src/test/java/com/devsuperior/dsmovie/services/MovieServiceTests.java
+++ src/test/java/com/devsuperior/dsmovie/services/MovieServiceTests.java
@@ -3,6 +3,7 @@ package com.devsuperior.dsmovie.services;
 import com.devsuperior.dsmovie.dto.MovieDTO;
 import com.devsuperior.dsmovie.entities.MovieEntity;
 import com.devsuperior.dsmovie.repositories.MovieRepository;
+import com.devsuperior.dsmovie.services.exceptions.ResourceNotFoundException;
 import com.devsuperior.dsmovie.tests.MovieFactory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,6 +19,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 
@@ -52,6 +54,7 @@ public class MovieServiceTests {
 		pageable = PageRequest.of(0, 12);
 
 		Mockito.when(repository.searchByTitle(any(), (Pageable) any())).thenReturn(page);
+		Mockito.when(repository.findById(existingID)).thenReturn(Optional.of(movie));
 	}
 
 	
@@ -67,6 +70,11 @@ public class MovieServiceTests {
 	
 	@Test
 	public void findByIdShouldReturnMovieDTOWhenIdExists() {
+		MovieDTO result = service.findById(existingID);
+
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(1L, result.getId());
+		Assertions.assertEquals(title, result.getTitle());
 	}
 	
 	@Test
